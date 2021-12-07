6ba80ecc98ade2fbbc4c60caf2369c188cd837f8
M0hammed-18 <Mohammed.Alweheid.elq.js@tuwaiq.edu.sa>
2021-12-07 07:24:06 +0300

    Challenge4Done

--- code_challenge/code-challenges/challenges-01.test.js
+++ code_challenge/code-challenges/challenges-01.test.js
@@ -66,10 +66,18 @@ Use `forEach` to build a new array of strings, each string modified by the callb
 
 const greeting = (word) => {
   // Solution code here...
+  const str= word.toUpperCase()+"!";
+  return str;
 };
 
 const speaker = (words, cb) => {
   // Solution code here...
+  let newstr=[];
+
+  words.forEach((element)=>{
+    newstr.push(cb(element))
+  })
+  return newstr
 };
 
 /* ------------------------------------------------------------------------------------------------
