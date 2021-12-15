99408d00bf61b42e0dd6a9688e67651f8041be39
M0hammed-18 <Mohammed.Alweheid.elq.js@tuwaiq.edu.sa>
2021-12-15 16:14:55 +0300

    Challenge024

--- code_challenge/code-challenges/challenges-02.test.js
+++ code_challenge/code-challenges/challenges-02.test.js
@@ -65,6 +65,7 @@ console.log(a) prints { fullName: 'Octavia Estelle Butler', yearBorn: 1947 }
 
 const addBirthYearProperty = (obj, year) => {
   // Solution code here...
+  obj.yearBorn=year
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -82,6 +83,7 @@ console.log(people[1].isAuthor) prints true
 
 const setStatusAsAuthor = (people) => {
   // Solution code here...
+  
 };
 
 /* ------------------------------------------------------------------------------------------------
